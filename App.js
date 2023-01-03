604c48ad2bc9fd3c8199bc99fd6fb9d0fb74ad2a
Jerry Ogunbor <ogunborjerry@gmail.com>
2023-01-03 17:41:38 +0100

    touchables

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello world</Text>
       <StatusBar style="auto" />
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          fadeDuration={1000}
+          //blurRadius={10}
+          source={{
+            height: 300,
+            width: 200,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
       {/* <Image source={require("./assets/icon.png")} /> */}
-      <Image
-        fadeDuration={1000}
-        blurRadius={10}
-        source={{
-          height: 300,
-          width: 200,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
     </SafeAreaView>
   );
 }
